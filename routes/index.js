5261b7fba2e2acbb91a6102ef9a9536e582835ec
frkandris <p.toth.andras@gmail.com>
2022-08-09 21:45:39 +0200

    list news items on the index page
    list news items on the index page

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,21 @@ var router = express.Router();
 let Parser = require('rss-parser');
 let parser = new Parser();
 
-(async () => {
-
-  let feed = await parser.parseURL('https://www.origo.hu/contentpartner/rss/origoall/origo.xml');
-  console.log(feed.title);
-
-  feed.items.forEach(item => {
-    console.log(item.title)
-  });
-
-})();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  (async () => {
+
+    let feed = await parser.parseURL('https://www.origo.hu/contentpartner/rss/origoall/origo.xml');
+    console.log(feed.title);
+  
+    feed.items.forEach(item => {
+      console.log(item.title)
+    });
+
+    res.render('index', { title: feed.title, items: feed.items });
+
+  })();
 });
 
 module.exports = router;
