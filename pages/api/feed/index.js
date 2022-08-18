9f1699dc2e922166e2a3e9ca10e1997899b1818d
frkandris <p.toth.andras@gmail.com>
2022-08-18 22:19:12 +0200

    check db if feed item already exists
    check db if feed item already exists

--- pages/api/feed/index.js
+++ pages/api/feed/index.js
@@ -13,9 +13,33 @@ export default async function handler(req, res) {
     case 'GET':
       try {
         const feed = await parser.parseURL('https://www.origo.hu/contentpartner/rss/origoall/origo.xml');
-        const options = { ordered: true };
-        const result = await FeedItem.insertMany(feed.items, options);
-        console.log(result);
+
+        // an async function that inserts the feed items into database
+        const insertFeedItems = async () => {
+
+          for (let i = 0; i < feed.items.length; i++) {
+            const item = feed.items[i];
+
+            // check if the feed item already exists in the database
+            const feedItemExists = await FeedItem.findOne({ guid: item.guid });
+            if (feedItemExists) {
+              return;
+            }
+            // if the feed item does not exist, insert it into the database
+            const feedItem = await FeedItem.create({
+              title: feed.items[0].title,
+              link: feed.items[0].link,
+              pubDate: feed.items[0].pubDate,
+              content: feed.items[0].content,
+              contentSnippet: feed.items[0].contentSnippet,
+              guid: feed.items[0].guid,
+              categories: feed.items[0].categories,
+              isoDate: feed.items[0].isoDate,
+            })
+          }
+        }
+        await insertFeedItems();
+
         res.status(200).json({ success: true, data: feed })
       } catch (error) {
         res.status(400).json({ success: false })
@@ -26,3 +50,4 @@ export default async function handler(req, res) {
       break
   }
 }
+
