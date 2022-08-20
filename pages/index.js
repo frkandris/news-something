a706872e5710944f72ed2bcc9469c9d5d77bf8ba
frkandris <p.toth.andras@gmail.com>
2022-08-20 22:55:04 +0200

    last 10 news, separate boxes for news sources
    last 10 news, separate boxes for news sources

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,49 @@
 import Link from 'next/link'
 import dbConnect from '../lib/dbConnect'
 import FeedItem from '../models/FeedItem'
-
+let moment = require('moment')
 let Parser = require('rss-parser');
 let parser = new Parser();
 
-const Index = ({ feed }) => (
-  <ul>
-     {feed.map((feedItem) => (
-       <li key={feedItem._id}><Link href={feedItem.link}>{feedItem.title}</Link> ({feedItem.feedTitle})</li>
-     ))}
-  </ul>
+const Index = ({ feedArray }) => (
+
+  <div>
+    {feedArray.map((feed, index) => (
+      <div key={index}>
+        <h3>{feed[0].feedTitle}</h3>
+        <ul>
+          {feed.map((item, index) => (
+            <li key={index}>
+              ({moment(item.pubDate).format('HH:mm')}) <Link href={item.link}>{item.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </div>
 )
 
 export async function getServerSideProps() {
 
   await dbConnect()
-  /* find all the data in our database */
-  const result = await FeedItem.find({})
-  const feed = result.map((doc) => {
-     const feed = doc.toObject()
-     feed._id = feed._id.toString()
-     return feed
-   })
-  return { props: { feed: feed } }
 
-  // const feed = await parser.parseURL('https://www.origo.hu/contentpartner/rss/origoall/origo.xml');
-  // console.log(feed);
-  // return { props: { feed: feed } }
+  let feedList = [
+    'ORIGO',
+    'Telex RSS: '
+  ];
+
+  let feedArray = [];
+  for (let i = 0; i < feedList.length; i++) {
+    // find the last 10 feed items for each feed
+    const result = await FeedItem.find({ feedTitle: feedList[i] }).sort({ pubDate: -1 }).limit(10);
+    const feed = result.map((doc) => {
+      const feed = doc.toObject()
+      feed._id = feed._id.toString()
+      return feed
+    })
+    feedArray.push(feed)
+  }
+  return { props: { feedArray: feedArray } }
 }
 
 export default Index
