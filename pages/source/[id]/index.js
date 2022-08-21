68a45ed88122271f8a3eb236988e7fbabec9a418
frkandris <p.toth.andras@gmail.com>
2022-08-21 21:54:59 +0200

    moved to subdirectory, added bootstrap design
    moved to subdirectory, added bootstrap design

---
+++ pages/source/[id]/index.js
@@ -0,0 +1,51 @@
+import { useRouter } from 'next/router'
+import dbConnect from '../../../lib/dbConnect'
+import Feed from '../../../models/Feed'
+import FeedItem from '../../../models/FeedItem'
+import Link from 'next/link'
+let moment = require('moment')
+
+const FeedPage = ({ feedData, feedItemList }) => {
+    const router = useRouter()
+    return (
+        <div className="container" key={feedData._id}>
+            <div className="row align-items-start m-2">
+                <div className="col border p-3 m-2 rounded bg-light">
+                    <h5>{feedData.displayTitle}</h5>
+                    <table className="table table-hover">
+                        <tbody>
+                            {feedItemList.map((item, index) => (
+                                <tr key={index}>
+                                    <td className="px-1 align-text-top">
+                                        {moment(item.pubDate).format('HH:mm')}
+                                    </td>
+                                    <td>
+                                        <Link href={item.link}>{item.title}</Link><br />
+                                        {item.content}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export async function getServerSideProps({ params }) {
+    await dbConnect()
+
+    const feedData = await Feed.findById(params.id).lean()
+    feedData._id = feedData._id.toString()
+
+    const result = await FeedItem.find({ feedTitle: feedData.title }).sort({ pubDate: -1 });
+    const feedItemList = result.map((doc) => {
+        const feedItemList = doc.toObject()
+        feedItemList._id = feedItemList._id.toString()
+        return feedItemList
+    })
+    return { props: { feedData: feedData, feedItemList: feedItemList } }
+}
+
+export default FeedPage
