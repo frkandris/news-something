223cd023903126c4fd41e040d91851faed69d985
frkandris <p.toth.andras@gmail.com>
2022-08-22 22:09:30 +0200

    remove feedList array, use db instead
    remove feedList array, use db instead

--- pages/api/feed/read.js
+++ pages/api/feed/read.js
@@ -1,5 +1,6 @@
 import dbConnect from '../../../lib/dbConnect'
 import FeedItem from '../../../models/FeedItem'
+import Feed from '../../../models/Feed'
 
 let Parser = require('rss-parser');
 let parser = new Parser();
@@ -7,24 +8,22 @@ let parser = new Parser();
 export default async function handler(req, res) {
   const { method } = req
 
-
   switch (method) {
     case 'GET':
       try {
         await dbConnect()
-        let feedList = [
-          'https://www.origo.hu/contentpartner/rss/origoall/origo.xml',
-          'https://telex.hu/rss/feed.rss',
-          'https://444.hu/feed',
-          'https://index.hu/24ora/rss/'
-        ];
+        let feedList = await Feed.find({});
+        let feedUrlArray = [];
         for (let i = 0; i < feedList.length; i++) {
-          let feed = await parser.parseURL(feedList[i]);
+          feedUrlArray.push(feedList[i].feedUrl);
+        }
+        for (let i = 0; i < feedUrlArray.length; i++) {          
+          let feed = await parser.parseURL(feedUrlArray[i]);
           const insertFeedItems = async () => {
             for (let i = 0; i < feed.items.length; i++) {
               const item = feed.items[i];
-              // console.log(item.title, feed.title);
               const feedItemExists = await FeedItem.findOne({ link: item.link });
+              // console.log("%s - %s", feed.title, item.title);
               if (!feedItemExists) {
                 const feedItem = await FeedItem.create({
                   title: item.title,
