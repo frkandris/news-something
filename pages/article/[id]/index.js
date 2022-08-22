efe32ad0816356a34f3174a5c37d078bcaba848b
frkandris <p.toth.andras@gmail.com>
2022-08-22 07:31:39 +0200

    title --> head, h5 --> h1
    title --> head, h5 --> h1

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 import dbConnect from '../../../lib/dbConnect'
 import FeedItem from '../../../models/FeedItem'
 import Link from 'next/link'
@@ -6,20 +7,27 @@ import Link from 'next/link'
 const ArticlePage = ({ feedItem }) => {
     const router = useRouter()
     return (
-        <div className="container" key={feedItem._id}>
-            <div className="row align-items-start m-2">
-                <div className="col border p-3 m-2 rounded bg-light">
-                    <h5 className="mb-0">{feedItem.title}</h5>
-                    <p><Link href={feedItem.link}>{feedItem.link}</Link></p>
-                    <p>{feedItem.content}</p>
-                    <div className="text-end">
-                        <Link href="/">
-                            <a>Vissza a főoldalra</a>
-                        </Link>
+        <>
+            <Head>
+                <title>
+                    {`${feedItem.title} | Friss hírek | friss-hirek.com`}
+                </title>
+            </Head>
+            <div className="container" key={feedItem._id}>
+                <div className="row align-items-start m-2">
+                    <div className="col border p-3 m-2 rounded bg-light">
+                        <h1 className="mb-0">{feedItem.title}</h1>
+                        <p><Link href={feedItem.link}>{feedItem.link}</Link></p>
+                        <p>{feedItem.content}</p>
+                        <div className="text-end">
+                            <Link href="/">
+                                <a>Vissza a főoldalra</a>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
