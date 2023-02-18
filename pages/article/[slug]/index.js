c43d04d2241c4b0987153a16b6efc1e2961e5f9b
frkandris <p.toth.andras@gmail.com>
2023-02-19 00:09:29 +0100

    more error handling (not really needed)
    more error handling (not really needed)

--- pages/article/[slug]/index.js
+++ pages/article/[slug]/index.js
@@ -5,56 +5,76 @@ import Link from 'next/link'
 
 const ArticlePage = ({ feedItem }) => {
 
-    return (
-        <>
-            <Head>
-                <title>
-                    {`${feedItem.title} | Friss hírek | friss-hirek.com`}
-                </title>
-            </Head>
-            <div className="container" key={feedItem._id}>
-                <div className="row align-items-start m-2">
-                    <div className="col border p-3 m-2 rounded bg-light">
-                        <h1 className="mb-0">{feedItem.title}</h1>
-                        <p className="alas"><a href={feedItem.link}>{feedItem.link}</a></p>
-                        <p dangerouslySetInnerHTML={{ __html: feedItem.content }} />
-                        <div className="text-end">
-                            <Link href="/">
-                                <a>Vissza a főoldalra</a>
-                            </Link>
+    if (feedItem === null) {
+        return (
+            <>
+                <Head>
+                    <title>
+                        {`Üres oldal | Friss hírek | friss-hirek.com`}
+                    </title>
+                </Head>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <Head>
+                    <title>
+                        {`${feedItem?.title} | Friss hírek | friss-hirek.com`}
+                    </title>
+                </Head>
+                <div className="container" key={feedItem?._id}>
+                    <div className="row align-items-start m-2">
+                        <div className="col border p-3 m-2 rounded bg-light">
+                            <h1 className="mb-0">{feedItem?.title}</h1>
+                            <p className="alas"><a href={feedItem?.link}>{feedItem?.link}</a></p>
+                            <p dangerouslySetInnerHTML={{ __html: feedItem?.content }} />
+                            <div className="text-end">
+                                <Link href="/">
+                                    <a>Vissza a főoldalra</a>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    }
 }
 
-// export async function getStaticPaths() {
-//     await dbConnect()
-//     const feedItemList = await FeedItem.find({})
-//     const paths = feedItemList.map(feedItem => ({
-//         params: {
-//             slug: feedItem.slug || ''
-//         }
-//     }))
-//     return {
-//         paths,
-//         fallback: 'blocking'
-//     }
-// }
+export async function getStaticPaths() {
+    await dbConnect()
+    // const feedItemList = await FeedItem.find().skip(128335).limit(5)
+    const feedItemList = await FeedItem.find({ slug: { $regex: /^[a-zA-Z0-9\-]+$/ } })
+    const paths = feedItemList.map(feedItem => ({
+        params: {
+            slug: feedItem.slug || ''
+        }
+    }))
+    return {
+        paths,
+        fallback: true
+    }
+}
 
-export async function getServerSideProps({ params }) {
-// export async function getStaticProps({ params }) {
+// export async function getServerSideProps({ params }) {
+export async function getStaticProps({ params }) {
     await dbConnect()
     const feedItem = await FeedItem.findOne({ slug: params.slug }).select('_id title link content').lean();
-    feedItem._id = feedItem._id.toString()
-    return {
-        props: {
-            feedItem: feedItem
+    if (feedItem) {
+        feedItem._id = feedItem._id.toString()
+        return {
+            props: {
+                feedItem: feedItem
+            },
+            revalidate: 60
         }
-        // },
-        // revalidate: 60
+    } else {
+        return {
+            props: {
+                feedItem: null
+            }
+        }
     }
 }
 
