e28d0be6628cedfc87a375894c5fb4720fef5581
frkandris <p.toth.andras@gmail.com>
2023-02-19 10:09:51 +0100

    github pages hosting
    github pages hosting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 // next.config.js
+
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
     // time in seconds of no pages generating during static
     // generation before timing out
     staticPageGenerationTimeout: 1000,
+
+    assetPrefix: isProd ? '/news-something/' : '',
+    images: {
+        unoptimized: true,
+    },
 }
