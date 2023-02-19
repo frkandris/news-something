73a4ac016594d5149849497d905d1fd5efd6af1d
frkandris <p.toth.andras@gmail.com>
2023-02-19 10:21:16 +0100

    no sitemap
    no sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,12 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://friss-hirek.com',
-    generateRobotsTxt: true,
-    sitemapSize: 5000,
-    exclude: ['/server-sitemap-index.xml'],
-    robotsTxtOptions: {
-      additionalSitemaps: [
-        'https://friss-hirek.com/server-sitemap-index.xml',
-      ],
-    },
-  }
+// /** @type {import('next-sitemap').IConfig} */
+// module.exports = {
+//     siteUrl: 'https://friss-hirek.com',
+//     generateRobotsTxt: true,
+//     sitemapSize: 5000,
+//     exclude: ['/server-sitemap-index.xml'],
+//     robotsTxtOptions: {
+//       additionalSitemaps: [
+//         'https://friss-hirek.com/server-sitemap-index.xml',
+//       ],
+//     },
+//   }
